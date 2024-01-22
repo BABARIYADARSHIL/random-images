9a6a7a3fd066df60703055294b6dff32260db4d6
BABARIYADARSHIL <darshilbabariya444@gmail.com>
2024-01-22 16:29:23 +0530

    first commit

---
+++ js/app.js
@@ -0,0 +1,17 @@
+image_array = [
+    '1.jpg',
+    '2.jpg',
+    '3.jpg',
+    '4.jpg',
+    '5.jpg',
+    '6.jpg',
+    '7.jpg'
+]
+
+function get_random_image(){
+    random_index = Math.floor(Math.random() * image_array.length); 
+
+    Selected_image = image_array[random_index]
+
+    document.getElementById('image_shower').src = `./images/${Selected_image}`
+}
